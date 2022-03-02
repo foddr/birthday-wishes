5e657132a5bb5dd0882d44f31297d3dea7a83f1a
foddr <90126813+foddr@users.noreply.github.com>
2022-03-02 18:32:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio")
+
+function new_image()
+{
+	fabric.Image.fromURL("BirthdayImage.jpg",function(Img){
+        
+        player_object=Img;
+        player_object.scaleToWidth(700);
+        player_object.scaleToHeight(510);
+        player_object.set ({
+            top:0,
+            left:0
+
+        
+            
+        });
+        canvas.add(block_img_object); 
+    });
+
+}
+
+
+function playSound(){
+	function playSound(){
+        x.play();
+    }
+}
